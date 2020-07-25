bcf62293991ee1d08c7226284dc5ba9f92efc836
Callum Bird <calbird97@outlook.com>
2020-07-25 17:52:15 +0100

    Fixed a lot of the small errors and confusion over array lists, but still need to fully clean up as well as a while finally error.

--- TicTacToe.java
+++ TicTacToe.java
@@ -6,6 +6,13 @@ public class TicTacToe {
     public static void main(String[] args) {
 
         //set of print statements to explain how to play the game
+        introduction();
+
+        //starts either a human vs human game or a computer vs human game
+        gameSet();
+    }
+
+    public static void introduction() {
         System.out.println("Welcome to TicTacToe");
         System.out.println("Each turn will be started with a print out of the current board state, which will look like this: ");
         ArrayList<Integer> exampleGameMoveList = new ArrayList<Integer>();
@@ -16,13 +23,59 @@ public class TicTacToe {
         printBoard(exampleGameMoveList);
         System.out.println("After this, each player will select a move to make using number keys 0-8, as shown below: ");
         printBoardExample();
-
-        //actual game method start
-        runGame();
     }
 
-    public static void runGame() {
+    public static void gameSet() {
+        int input = playerInput("Enter a number to select a game: \n1. Human vs Human \n2. Computer vs Human");
+
+        int[] choices = {1,2};
+        ArrayList<Integer> availableMoves = makeArrayList(choices);
+        
+        boolean possibleChoice = checkLegalMove(input, availableMoves);
+        while (!possibleChoice) {
+            try {
+                input = playerInput("Your choice was invalid. Please choose again");
+                possibleChoice = checkLegalMove(input, availableMoves);
+            }
+
+            finally {
+                if (input == 1) {
+                    runHumanGame();
+                }
+                else {
+                    //runComputerGame();
+                }
+            }
+        }
+    }
+
+    public static ArrayList<Integer> makeArrayList(int[] array) {
+        ArrayList<Integer> arrayList = new ArrayList();
+        for (int i=0; i<array.length; i++) {
+            arrayList.add(i);
+        }
+        return arrayList;
+    }
+
+    public static boolean checkLegalMove(int move, ArrayList<Integer> availableMoves) {
+        boolean possibleChoice = false;
+
+        if(availableMoves.contains(move)) {
+            possibleChoice = true;
+        }
+
+        return possibleChoice;
+    }
+
+    public static ArrayList<Integer> updateGameList(int playerMove, ArrayList<Integer> gameList) {
+        gameList.add(playerMove);
+        return gameList;
+    }
+
+    public static void runHumanGame() {
         //initialise game list, which is the record of every player's move
+        int[] possibleMoves = {0,1,2,3,4,5,6,7,8};
+        ArrayList<Integer> possibleMovesList = makeArrayList(possibleMoves);
         ArrayList<Integer> gameList = new ArrayList<Integer>();
         
         for (int turns = 0; turns<9; turns++){
@@ -33,16 +86,15 @@ public class TicTacToe {
                 System.out.println("Player 2");
             }
 
-        int playerMove = playerMove();
-        boolean legalMove = checkMove(playerMove, gameList);
+        int playerMove = playerInput("Enter your move");
+        boolean legalMove = checkLegalMove(playerMove, possibleMovesList);
             while(!legalMove) {
                 try {
-                    System.out.println("That is not a legal move. Please pick a new move ");
-                    playerMove = playerMove();
-                    legalMove = checkMove(playerMove, gameList);
+                    playerMove = playerInput("That is not a legal move. Please pick a new move ");
+                    legalMove = checkLegalMove(playerMove, possibleMovesList);
                 }
                 finally {
-                gameList = updateGameList(playerMove, gameList);
+                gameList.add(playerMove);
                 }
             }
 
@@ -55,25 +107,25 @@ public class TicTacToe {
         System.out.println("Draw: no one wins this game.");
     }
 
+    public static int playerInput(String message) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(message);
+        int choice = input.nextInt();
+
+        return choice;
+    }
+
     public static void printBoard(ArrayList<Integer> gameMoveList) {
-        String[] gameBoard = new String[9];
-        int[] gameListArray = new int[gameMoveList.size()];
-        gameListArray = arrayListToArray(gameMoveList);
-        for(int i=0; i<9; i++) {
-            gameBoard[i] = " ";
-        }
+        String[] gameBoard = initialiseGameBoard();
 
-        System.out.println(gameListArray[0]);
-
-        for(int j=0; j<gameListArray.length; j++) {
-            if(j % 2 == 0) {
-                gameBoard[gameListArray[j]] = "X";
+        for(int i=0; i<gameMoveList.size(); i++) {
+            if(i % 2 == 0) {
+                gameBoard[gameMoveList.get(i)] = "X";
             }
             else {
-                gameBoard[gameListArray[j]] = "0";
+                gameBoard[gameMoveList.get(i)] = "0";
             }
         }
-
         System.out.println("             ");
 		System.out.println("| " + gameBoard[0] + " | " + gameBoard[1] + " | " + gameBoard[2] + " |");
 		System.out.println("|-----------|");
@@ -81,6 +133,15 @@ public class TicTacToe {
 		System.out.println("|-----------|");
         System.out.println("| " + gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8] + " |");
         System.out.println("             ");
+    }
+
+    public static String[] initialiseGameBoard() {
+        String[] gameBoard = new String[9];
+        for (int i=0; i<9; i++) {
+            gameBoard[i] = " ";
+        }
+
+        return gameBoard;
     }
 
     public static void printBoardExample() {
@@ -93,38 +154,6 @@ public class TicTacToe {
         System.out.println("| 6 | 7 | 8 |");
         System.out.println("             ");
 
-    }
-
-    public static int playerMove() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your move ");
-        int move = input.nextInt();
-
-        return move;
-    }
-
-    public static boolean checkMove(int move, ArrayList<Integer> gameList) {
-        boolean availableMove = false;
-        if (gameList.size() < 1) {
-            gameList.add(9);
-        }
-
-        if (move >=0 && move <= 8){
-        for (int i=0; i < gameList.size(); i++) {
-            if (move != gameList.get(i)) {
-                availableMove = true;
-            }
-            else {
-                availableMove = false;
-            }
-        }
-    }
-        return availableMove;
-    }
-
-    public static ArrayList<Integer> updateGameList(int playerMove, ArrayList<Integer> gameList) {
-        gameList.add(playerMove);
-        return gameList;
     }
 
     public static void checkWin(ArrayList<Integer> gameList){
@@ -213,18 +242,6 @@ public class TicTacToe {
         }
         return player2MoveList;
     }
-
-    public static int[] arrayListToArray(ArrayList<Integer> gameMoveList) {
-        int[] gameList = new int[gameMoveList.size()];
-
-        for (int i=0; i < gameList.length; i++) {
-            gameList[i] = gameMoveList.get(i);
-        }
-        
-        return gameList;
-    }
-
-    
 }
 
 
